3fb119e2dbab32e3343dae0c2dd52fd3f82ce3ce
Marcin <mieker@wp.pl>
2020-06-04 23:00:40 +0200

    init commit

---
+++ rational.cpp
@@ -0,0 +1,72 @@
+#include "rational.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    Rational r2(2, 11), r3(1, -3), r5(18, 6);
+    Rational res1 = 3 + r2 * r3;
+    Rational res2 = (3 + r2) * r3;
+    Rational res3 = 3 + r3 * (r2 + 2) / (r5 - r3);
+
+    cout << 3 << " + " << r2 << " * " <<  r3 << " = " << res1 << endl;
+    cout << "(" << 3 << " + " << r2 << ")" << " * " <<  r3 << " = " << res2 << endl;
+    cout << 3 << " + " << r3 << " * " << "(" << r2 << " + " << 2 << ")" << "/"
+    << "(" << r5 << " - " << r3 << ")" << " = " << res3 << endl;
+
+
+
+
+    ////////////////////////////////////////////////////
+//    Rational r41{3,4};
+//    Rational r42 = 2 / r41;
+//    cout << r42 << endl;
+
+//    cout << "Hello World!" << endl;
+//    Rational r1{2,3};
+//   cout << r1 << endl;
+
+//    Rational r2(r1);
+//    cout << r2 << endl;
+
+//    Rational r3 = r2;
+//    cout << "r3: " << r3 << endl;
+
+//    Rational r4(12);
+//    cout << r4 << endl;
+
+////    Rational r5;
+////    cin >> r5;
+////    cout << r5;
+
+//    Rational rA{3,4};
+//    Rational rB{5,4};
+//    Rational rC = rA + rB;
+//    cout << "3/4 + 5/4 = " << rC << endl;
+
+//    Rational rX{2,3};
+//    Rational rY{3,4};
+//    Rational rZ = rX + rY;
+//    cout << "2/3 + 3/4 = " << rZ << endl;
+
+//    Rational r11{3,4};
+//    Rational r12{1,4};
+//    Rational r13 = r11 - r12;
+//    cout << "3/4 - 1/4 = " << r13 << endl;
+
+//    Rational r21{3,5};
+//    Rational r22{1,2};
+//    Rational r23 = r21 * r22;
+//    cout << "3/5 * 1/2 = " << r23 << endl;
+
+//    Rational r31{4,7};
+//    Rational r32{2,1};
+//    Rational r33 = r31 / r32;
+//    cout << "6/6 / 2/1 = " << r33 << endl;
+    ////////////////////////////////////////////////////////
+
+
+
+    return 0;
+}
---
+++ rational.h
@@ -0,0 +1,166 @@
+#include <iostream>
+
+using namespace std;
+
+class Rational
+{
+public:
+   //konstruktor
+   Rational(int numerator = 0, int denominator = 1) : numer{numerator}, denom{denominator} {};
+   //destruktor
+   ~Rational() {}
+   //tworzy rational kopiujac podany w argumencie inny rational
+   Rational(const Rational& copy)
+   {
+       this->numer = copy.numerator();
+       this->denom = copy.denominator();
+   };
+   //tworzy rationala a w zasadzie nowe odniesienie do niego bo beda mialy ten sam memo space
+   Rational& operator=(const Rational& other)
+   {
+       if (&other != this) {
+         this->numer = other.numerator();
+         this->denom = other.denominator();
+       }
+       return *this;
+   };
+   //tworzy rational z inta czyli robi przykladowo z 12 na 12/1, z 6 na 6/1
+   Rational& operator=(int i)
+   {
+       this->numer = i;
+       this->denom = 1;
+       return *this;
+   };
+   //getter numer
+   int numerator() const { return numer; }
+   //getter denom
+   int denominator() const { return denom; }
+
+   static Rational normalize(const Rational& other);                            //tworzy static rational z innego rationala
+   static Rational normalized(int numerator, int denominator);                  //tworzy static rational z dwoch intow w postaci numeratora i denominatora
+                                                                                //byc moze chodzi tutaj o to ze normalizujemy l.rzeczywista np z 20/10 na 2 alby z 3/12 na 1/4
+   //dodawanie rational
+   Rational operator+(const Rational& other) const
+   {
+       if (this->denom == other.denom) {
+           return Rational {this->numer + other.numer, this->denom};
+       }
+       else {
+           int tempNumer = this->numer * other.denom + other.numer * this->denom;
+           int tempDenom = this->denom * other.denom;
+           return Rational {tempNumer, tempDenom};
+       }
+   };
+   //odejmowanie rational
+   Rational operator-(const Rational& other) const
+   {
+       if (this->denom == other.denom) {
+           return Rational {this->numer - other.numer, this->denom};
+       }
+       else {
+           int tempNumer = this->numer * other.denom - other.numer * this->denom;
+           int tempDenom = this->denom * other.denom;
+           return Rational {tempNumer, tempDenom};
+       }
+   };
+   //mnozenie rational
+   Rational operator*(const Rational& other) const
+   {
+       int tempNumer = this->numer * other.numer;
+       int tempDenom = this->denom * other.denom;
+       return Rational {tempNumer, tempDenom};
+   };
+   //dzielenie rational
+   Rational operator/(const Rational& other) const
+   {
+       int tempNumer = this->numer * other.denom;
+       int tempDenom = this->denom * other.numer;
+       return Rational {tempNumer, tempDenom};
+   };
+
+   Rational operator+() const;                                             //?? //nie rozumiem
+   Rational operator-() const;
+
+   bool operator==(const Rational& other) const;                                //zwraca booleana uzywajac operatory logiczne porownujac dwa rationale
+   bool operator>=(const Rational& other) const;
+   bool operator<=(const Rational& other) const;
+   bool operator>(const Rational& other) const;
+   bool operator<(const Rational& other) const;
+
+private:
+   int numer;
+   int denom;
+   //greatest common divisor - najwiekszy wspolny dzielnik
+   int gcdND;
+
+   void initialize(int numerator, int denominator);                        //?? //void ktory inicjalizuje cos z liczba rzeczywista ale nie wiem dokladnie o co kaman
+
+   //cout
+   friend std::ostream& operator<<(std::ostream &os, const Rational &r)
+   {
+      os << r.numer << "/" << r.denom;
+      return os;
+   };
+   //cin
+   friend std::istream& operator>>(std::istream &is, Rational &r)
+   {
+       cout << "numerator: ";
+       is >> r.numer;
+       cout << "denominator: ";
+       is >> r.denom;
+       return is;
+   };
+
+};
+
+Rational operator+(const Rational& r, int i)
+{
+    int tempNumer = r.numerator() + i * r.denominator();
+    int tempDenom = r.denominator();
+    return Rational {tempNumer, tempDenom};
+};
+Rational operator+(int i, const Rational& r)
+{
+    int tempNumer = r.numerator() + i * r.denominator();
+    int tempDenom = r.denominator();
+    return Rational {tempNumer, tempDenom};
+};
+
+Rational operator-(const Rational& r, int i)
+{
+    int tempNumer = r.numerator() - i * r.denominator();
+    int tempDenom = r.denominator();
+    return Rational {tempNumer, tempDenom};
+};
+Rational operator-(int i, const Rational& r)
+{
+    int tempNumer = i * r.denominator() - r.numerator();
+    int tempDenom = r.denominator();
+    return Rational {tempNumer, tempDenom};
+};
+
+Rational operator*(const Rational& r, int i)
+{
+    int tempNumer = r.numerator() * i;
+    int tempDenom = r.denominator();
+    return Rational {tempNumer, tempDenom};
+};
+Rational operator*(int i, const Rational& r)
+{
+    int tempNumer = r.numerator() * i;
+    int tempDenom = r.denominator();
+    return Rational {tempNumer, tempDenom};
+};
+
+Rational operator/(const Rational& r, int i)
+{
+    int tempNumer = r.numerator();
+    int tempDenom = r.denominator() * i;
+    return Rational {tempNumer, tempDenom};
+};
+Rational operator/(int i, const Rational& r)
+{
+    int tempNumer = i * r.denominator();
+    int tempDenom = r.numerator();
+    return Rational {tempNumer, tempDenom};
+};
