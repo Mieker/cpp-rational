28318575ddcee0273e79961b4100e67297efc50b
Marcin <mieker@wp.pl>
2020-06-05 22:13:30 +0200

    code cleared

--- rational.h
+++ rational.h
@@ -5,42 +5,37 @@ using namespace std;
 class Rational
 {
 public:
-   //konstruktor
    Rational(int numerator = 0, int denominator = 1)
    {
        initialize(numerator, denominator);
    };
-   //destruktor
+
    ~Rational() {}
-   //tworzy rational kopiujac podany w argumencie inny rational
+
    Rational(const Rational& copy)
    {
        this->numer = copy.numerator();
        this->denom = copy.denominator();
    };
-   //tworzy rationala a w zasadzie nowe odniesienie do niego bo beda mialy ten sam memo space
-   Rational& operator=(const Rational& other)                                                                   //
+
+   Rational& operator=(const Rational& other)
    {
        if (&other != this) {
-
          this->numer = other.numerator();
          this->denom = other.denominator();
        }
        return *this;
    };
-   //tworzy rational z inta czyli robi przykladowo z 12 na 12/1, z 6 na 6/1
-   Rational& operator=(int i)                                                                                   //
+
+   Rational& operator=(int i)
    {
        this->numer = i;
        this->denom = 1;
        return *this;
    };
-   //getter numer
+
    int numerator() const { return numer; }
-   //getter denom
    int denominator() const { return denom; }
-   //getter gcdND - //////////////////////////////do usuniecia
-   int getGCD() const { return gcdND; }
 
    static Rational normalize(const Rational& other)
    {
@@ -51,7 +46,7 @@ public:
    {
        return Rational::normalize(Rational(numerator, denominator));
    };
-   //dodawanie rational
+
    Rational operator+(const Rational& other) const
    {
        if (this->denom == other.denom) {
@@ -63,7 +58,7 @@ public:
            return normalize(Rational {tempNumer, tempDenom});
        }
    };
-   //odejmowanie rational
+
    Rational operator-(const Rational& other) const
    {
        if (this->denom == other.denom) {
@@ -75,14 +70,14 @@ public:
            return normalize(Rational {tempNumer, tempDenom});
        }
    };
-   //mnozenie rational
+
    Rational operator*(const Rational& other) const
    {
        int tempNumer = this->numer * other.numer;
        int tempDenom = this->denom * other.denom;
        return normalize(Rational {tempNumer, tempDenom});
    };
-   //dzielenie rational
+
    Rational operator/(const Rational& other) const
    {
        int tempNumer = this->numer * other.denom;
@@ -90,7 +85,6 @@ public:
        return normalize(Rational {tempNumer, tempDenom});
    };
 
-   //umozliwia zapis Rational r w postaci -r lub +r
    Rational operator+() const
    {
        return *this*(+1);
@@ -100,7 +94,7 @@ public:
        return *this*(-1);
    };
 
-   bool operator==(const Rational& other) const;                                //zwraca booleana uzywajac operatory logiczne porownujac dwa rationale
+   bool operator==(const Rational& other) const;
    bool operator>=(const Rational& other) const;
    bool operator<=(const Rational& other) const;
    bool operator>(const Rational& other) const;
@@ -116,10 +110,7 @@ public:
 private:
    int numer;
    int denom;
-   //greatest common divisor - najwiekszy wspolny dzielnik
    int gcdND;
-
-
 
    void initialize(int numerator, int denominator)
    {
@@ -134,12 +125,9 @@ private:
        this->gcdND = findGCD(abs(numerator), abs(denominator));
    };
 
-   //cout
    friend std::ostream& operator<<(std::ostream &os, const Rational &rr)
    {
-//      Rational rr = normalize(r);
-
-       if (rr.numer < 0) {
+      if (rr.numer < 0) {
           os << "(" << rr.numer << "/" << rr.denom << ")";
       }
       else {
@@ -147,7 +135,7 @@ private:
       }
       return os;
    };
-   //cin
+
    friend std::istream& operator>>(std::istream &is, Rational &r)
    {
        cout << "numerator: ";
@@ -210,7 +198,3 @@ Rational operator/(int i, const Rational& r)
     int tempDenom = r.numerator();
     return Rational {tempNumer, tempDenom};
 };
-
-
-
-// liczby calkowite ustaw wyswietlanie poprzez cout -> if denom = 1 os = numer
